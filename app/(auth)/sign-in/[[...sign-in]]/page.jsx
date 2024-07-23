4857721a4041bc2281e9c8877e2fed915942496b
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-07-23 17:29:15 +0530

    add ui for signup and signin

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return(<div className="border border-red-900"> <SignIn /></div>);
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 }
