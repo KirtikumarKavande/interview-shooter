d86208d3be398b02dd868d3d297ed78df3e861b5
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-08-06 19:02:19 +0530

    fix:redirect to dashboard page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+'use client'
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
+ const router= useRouter()
+  useEffect(()=>{
+    router.push('/dashboard')
+  },[])
+
 }
