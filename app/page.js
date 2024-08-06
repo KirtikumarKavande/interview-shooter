7cb12b4db529c411ddc660b7f43eeb6274aad999
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-08-06 21:57:54 +0530

    style:add spinner before loading

--- app/page.js
+++ app/page.js
@@ -9,7 +9,13 @@ export default function Home() {
   },[])
   return (
     <div>
-      Loading...
+     <div class="flex items-center justify-center h-screen">
+    <div class="relative">
+        <div class="h-36 w-36 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+        <div class="absolute top-0 left-0 h-36 w-36 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+        </div>
+    </div>
+</div>
     </div>
   )
 }
