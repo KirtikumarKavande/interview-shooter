fcb6f5cc7d7752284599abf8045c190009a0a392
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-08-06 18:23:30 +0530

    feature:add interview landing page

--- app/_components/InterviewItemCard.jsx
+++ app/_components/InterviewItemCard.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { dateConverter } from '../_utilis/helper'
 import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 
 const InterviewItemCard = ({interviewInfo}) => {
     console.log(interviewInfo)
+   const router= useRouter()
   return (
     <div className='border shadow-sm rounded-lg p-3'>
         <h2 className='font-bold text-blue-800'>{interviewInfo?.jobPosition}</h2>
         <h2 className='text-sm text-gray-600'>{interviewInfo?.jobExperience}Years of Experience</h2>
         <h2 className='text-gray-400'>Created At:{dateConverter(interviewInfo?.createdAt)}</h2>
         <div className='flex justify-between mt-2'>
-            <Button size='sm' variant='outline'>Feedback</Button>
-            <Button size='sm'className="bg-blue-600 text-white">Retake</Button>
+            <Button size='sm' variant='outline' onClick={() => router.push(`/dashboard/interview/${interviewInfo?.mockId}/feedback`)}>Feedback</Button>
+            <Button size='sm'className="bg-blue-600 text-white"
+            onClick={() => router.push(`/dashboard/interview/${interviewInfo?.mockId}`)}
+            >Retake</Button>
         </div>
     </div>
   )
--- app/dashboard/interview/[interviewid]/page.jsx
+++ app/dashboard/interview/[interviewid]/page.jsx
@@ -1,13 +1,19 @@
 "use client";
 import { addInterviewInfo } from "@/app/_store/interviewInfo.slice";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
-import Link from 'next/link';
-import React, { useEffect } from "react";
+import { WebcamIcon } from "lucide-react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Webcam from "react-webcam";
 
 const Interview = ({ params }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [webcamEnabled, setWebcamEnabled] = useState(false);
+  const interViewInfo = useSelector((state) => state.interviewInfo);
+  console.log("redux store", interViewInfo);
   useEffect(() => {
     if (params?.interviewid) {
       getQuestionAndAnswer();
@@ -18,16 +24,57 @@ const Interview = ({ params }) => {
       const response = await axios.get(
         `http://localhost:3000/api/addjobinfo/${params.interviewid}`
       );
-      dispatch(addInterviewInfo(response.data))
+      dispatch(addInterviewInfo(response.data));
     } catch (error) {
       toast.error("something went wrong");
     }
   }
   return (
     <div>
-      <Link href={`/dashboard/interview/${params.interviewid}/start`}>
-        start Interview
-      </Link>
+      <div className=""></div>
+      <div className="my-10 flex justify-center flex-col items-center">
+        <h2 className="font-bold text-2xl">Let's Get Started</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10  ">
+          <div className="flex flex-col my-5 gap-5 p-5 rounded-lg border">
+            <h2 className="text-lg">
+              <strong>Job Role/Job Position</strong>:
+              {interViewInfo?.jobPosition}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Job Description/Tech Stack</strong>:
+              {interViewInfo?.jobDesc}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Years of Experience </strong>:
+              {interViewInfo?.jobExperience}
+            </h2>
+          </div>
+          <div>
+            {webcamEnabled ? (
+              <Webcam
+                onUserMedia={() => setWebcamEnabled(true)}
+                onUserMediaError={() => setWebcamEnabled(false)}
+                mirrored
+                style={{
+                  height: 300,
+                  width: 300,
+                }}
+              />
+            ) : (
+              <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border " />
+            )}
+          </div>
+        </div>
+        <div className="flex space-x-8">
+          <Button onClick={() => setWebcamEnabled(true)}>Enable Web Cam</Button>
+          <Button>
+            {" "}
+            <Link href={`/dashboard/interview/${params.interviewid}/start`}>
+              start Interview
+            </Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
