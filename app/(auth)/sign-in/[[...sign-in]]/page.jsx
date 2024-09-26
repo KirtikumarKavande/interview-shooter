bf11e34fdb132e38e548caf7577de8600959388a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-26 15:46:38 +0530

    add dummy credential for test

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,6 +3,10 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div>
+      <div className="pl-9 text-gray-600 mb-4">
+        <div>Email: yuvrajdhonisachin@gmail.com</div>
+        <div>Password: Kirtikumar@1</div>
+      </div>
       <SignIn />
     </div>
   );
